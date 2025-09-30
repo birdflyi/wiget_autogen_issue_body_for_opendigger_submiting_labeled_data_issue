c44254796b189141e6ddd8d91b368b7a28838340
birdflyi <1065168006@qq.com>
2025-10-01 00:17:30 +0800

    Update Data: Update data up to 20250930.

--- main.py
+++ main.py
@@ -831,6 +831,7 @@ if __name__ == '__main__':
         "dbfeatfusion_records_202505_automerged_manulabeled.csv",
         "dbfeatfusion_records_202507_automerged_manulabeled.csv",
         "dbfeatfusion_records_202508_automerged_manulabeled.csv",
+        "dbfeatfusion_records_202509_automerged_manulabeled.csv",
     ]
     # dynamic settings
     idx_last_v = -2
